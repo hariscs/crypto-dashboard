0e000752885c0ad4cda6b3e20d71f603edf0188c
Haris Shah <harisshah385@gmail.com>
2022-01-10 23:34:00 +0500

    add: sidebar icons add

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -4,6 +4,28 @@ const Sidebar = () => {
 	return (
 		<div>
 			<Logo bgcolor='blue' clr='white' />
+			<ul>
+				<li>
+					<i class='fas fa-home'></i>
+					<a href='/'>Home</a>
+				</li>
+				<li>
+					<i class='fas fa-chart-bar'></i>
+					<a href='/'>Exchange</a>
+				</li>
+				<li>
+					<i class='fas fa-chart-line'></i>
+					<a href='/'>EasyBuy</a>
+				</li>
+				<li>
+					<i class='fas fa-comments'></i>
+					<a href='/'>C2C</a>
+				</li>
+				<li>
+					<i class='fas fa-chart-pie'></i>
+					<a href='/'>Asset</a>
+				</li>
+			</ul>
 		</div>
 	);
 };
